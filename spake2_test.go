3062175964c820d17c7807b1039a9f9d472118bc
ValiantChip <egb154@gmail.com>
2025-09-10 20:06:59 -0400

    added test

---
+++ spake2_test.go
@@ -0,0 +1,101 @@
+package gospake2
+
+import (
+	"crypto/rand"
+	"slices"
+	"testing"
+)
+
+type SpakeTest struct {
+	name      string
+	passwordA string
+	passwordB string
+	valid     bool
+}
+
+var tests = []SpakeTest{
+	{
+		name:      "valid password",
+		passwordA: "password",
+		passwordB: "password",
+		valid:     true,
+	},
+	{
+		name:      "invalid password",
+		passwordA: "password",
+		passwordB: "password2",
+		valid:     false,
+	},
+}
+
+func TestBasic(t *testing.T) {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			alice := NewA([]byte(test.passwordA), "A", "B", rand.Reader)
+			bob := NewB([]byte(test.passwordB), "A", "B", rand.Reader)
+
+			pA, err := alice.Start()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			pB, err := bob.Start()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			k, cA, err := alice.Finish(pB)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			k2, cB, err := bob.Finish(pA)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			kv := slices.Equal(k, k2)
+
+			av, err := alice.Verify(cB)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			bv, err := bob.Verify(cA)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if test.valid {
+				if !kv {
+					t.Log("Keys do not match")
+					t.Fail()
+				}
+
+				if !av {
+					t.Log("B confirmation messages did not match")
+					t.Fail()
+				}
+
+				if !bv {
+					t.Log("A confirmation messages did not match")
+					t.Fail()
+				}
+			} else {
+				if kv {
+					t.Log("Keys should not match")
+					t.Fail()
+				}
+				if bv {
+					t.Log("B confirmation message should not match")
+					t.Fail()
+				}
+
+				if av {
+					t.Log("A confirmation messages should not match")
+					t.Fail()
+				}
+			}
+		})
+	}
+}
