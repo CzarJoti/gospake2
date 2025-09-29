892446e27ebb67b0c14e03070ea1fe84bb52224d
ValiantChip <egb154@gmail.com>
2025-09-28 21:16:11 -0400

    moved ciphersuite

--- protocol.go
+++ protocol.go
@@ -7,32 +7,6 @@ import (
 	"io"
 )
 
-// CipherSuite is a ciphersuite for the Spake2 protocol
-type CipherSuite[S Scalar[S], W Point[W, S], G Group[W, S]] struct {
-	Group        G
-	Hash         func() hash.Hash
-	PasswordHash func() hash.Hash
-	Kdf          func(func() hash.Hash, []byte, []byte, string, int) ([]byte, error)
-	Mac          func(func() hash.Hash, []byte) hash.Hash
-}
-
-// Creates a new CipherSuite
-func NewCipherSuite[S Scalar[S], W Point[W, S], G Group[W, S]](
-	group G,
-	hash func() hash.Hash,
-	passwordHash func() hash.Hash,
-	kdf func(func() hash.Hash, []byte, []byte, string, int) ([]byte, error),
-	mac func(func() hash.Hash, []byte) hash.Hash,
-) CipherSuite[S, W, G] {
-	return CipherSuite[S, W, G]{
-		Group:        group,
-		Hash:         hash,
-		PasswordHash: passwordHash,
-		Kdf:          kdf,
-		Mac:          mac,
-	}
-}
-
 type protocol[S Scalar[S], W Point[W, S], G Group[W, S]] struct {
 	g      G
 	hsh    func() hash.Hash
